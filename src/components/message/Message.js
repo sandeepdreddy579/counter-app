dae40b30d66a8a2bdc2178e2ca66d6e75a179814
sandeepdreddy579 <sandeepdreddy94@gmail.com>
2025-11-04 17:29:31 +0530

    exactly word change

--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Message = () => {
-  console.log("message component rendering   ");
+  console.log("message component rendering  exactly  ");
   return (
     <>
       <h1>Message</h1>;
